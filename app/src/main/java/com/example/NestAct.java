60936a5bd73aa3cb4347459b7dde18b3247f93c7
iWatching <21413244@qq.com>
2019-04-10 09:55:25 +0800

    略

---
+++ app/src/main/java/com/example/NestAct.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NestAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nest_act);
+    }
+}
